f01ea5bba15ee96adc50560a981a85feac8b3da7
rajswi19 <rajswi19@gmail.com>
2025-07-29 20:05:04 +0530

    fetaure 2 complte

--- components/FeatureSection3.tsx
+++ components/FeatureSection3.tsx
@@ -2,28 +2,28 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Globe, Shield, Users, Wallet } from "lucide-react";
 const featureCards = [
   {
-    title: "Do more with NFTs — pin, hide, burn, and list. .",
+    title: "Scam detection flags malicious transactions instantly. ",
     description:
       "No more switching. Manage all your assets across different networks seamlessly.",
-    bgColor: "bg-indigo-100/50",
+    bgColor: "bg-red-200",
     imgSrc:
-      "https://sanity-proxy-v2.phantom.app/files/3nm6d03a/production/c0004d979a7981cb1e78388368da034570af528d.webm#t=0.1",
+      "https://sanity-proxy-v2.phantom.app/files/3nm6d03a/production/ef9e9258b2cb966a1748ef70d71706c171856e14.webm#t=0.1",
   },
   {
-    title: " Swap tokens super fast and at low fees.",
+    title: " Self-custodial means you control your funds. We never have access. ",
     description:
       "Seamlessly browse, buy, and showcase your favorite digital collectibles.",
-    bgColor: "bg-rose-100/50",
+    bgColor: "bg-yellow-100",
     imgSrc:
-      "https://sanity-proxy-v2.phantom.app/files/3nm6d03a/production/284feb2572b3aaca8545e290aa5634797afe1965.webm#t=0.1",
+      "https://sanity-proxy-v2.phantom.app/files/3nm6d03a/production/814e142cae1b3da7bd7455709d3ab8674bbb734d.webm#t=0.1",
   },
   {
-    title: "Store, stake, and earn rewards with your tokens.",
+    title: "Connect your Ledger to keep your crypto even safer. ",
     description:
       "A beautiful interface to display your unique digital art and assets.",
-    bgColor: "bg-amber-100/50",
+    bgColor: "bg-blue-200",
     imgSrc:
-      "https://sanity-proxy-v2.phantom.app/files/3nm6d03a/production/6e7ae33899041a368becdff9b35708a5a2a29a9d.webm#t=0.1",
+      "https://sanity-proxy-v2.phantom.app/files/3nm6d03a/production/0f1866916249a4bf118507a135c7ba233f48e91a.webm#t=0.1",
   },
 ];
 
@@ -34,22 +34,22 @@ export default function FeaturesSection() {
         <div className="relative">
           <div className="relative z-10 text-center mb-12">
             <h2 className="text-6xl md:text-8xl font-medium tracking-tight text-purple-900  leading-[125%]">
-              Keep everything <br />
-              <div className=" flex gap-3 justify-center">
+              Controlled by you, <br />
+              <div className=" flex gap-1.5 justify-center">
                 {" "}
-                in{" "}
+               secured
                 <img
                   src="/blueberry-logo.png"
                   height={36}
                   width={100}
-                  className=" aspect-square rotate-30"
+                  className=" aspect-square rotate-28"
                 />
-                one place
+                by us
               </div>
             </h2>
           </div>
-          <div className="bg-white text-purple-950 rounded-full text-2xl font-medium mb-14 mt-46 ml-6 flex gap-3 items-center w-fit py-4 px-5">
-            <Globe /> <p>Your web3 tools</p>
+          <div className="bg-white text-black rounded-full text-2xl font-medium mb-14 mt-46 ml-6 flex gap-3 items-center w-fit py-4 px-5">
+            <Shield /> <p>Your security</p>
           </div>
           <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]">
             <ul className="flex items-center justify-center md:justify-start [&_li]:mx-4 animate-infinite-scroll hover:[animation-play-state:paused]">
@@ -100,8 +100,7 @@ const FeatureCard = ({
     />
     </div>
     <div >
-      <h3 className="text-xl font-bold text-purple-800">{title}</h3>
-      <p className="text-purple-500 text-xs mt-2">{description}</p>
+      <h3 className="text-xl font-bold text-purple-950 text-center">{title}</h3>
     </div>
     <div></div>
   </Card>
